a490265046534fec515ec4cf466e07a17b55bd04
animashachka-afk <animashachka@gmail.com>
2026-08-12 00:04:05 +0500

    Update imagekit-auth.js

--- api/imagekit-auth.js
+++ api/imagekit-auth.js
@@ -1,13 +1,6 @@
 const crypto = require("crypto");
 
 module.exports = function handler(req, res) {
-
-    if (req.method !== "GET") {
-        return res.status(405).json({
-            error: "Method not allowed"
-        });
-    }
-
     const privateKey = process.env.IMAGEKIT_PRIVATE_KEY;
 
     if (!privateKey) {
@@ -18,18 +11,20 @@ module.exports = function handler(req, res) {
 
     const token = crypto.randomUUID();
 
-    const expire =
-        Math.floor(Date.now() / 1000) + 2400;
+    const expire = Math.floor(Date.now() / 1000) + 1800;
 
-    const signature =
-        crypto
-            .createHmac("sha1", privateKey)
-            .update(token + expire)
-            .digest("hex");
+    const signature = crypto
+        .createHmac("sha1", privateKey)
+        .update(token + expire)
+        .digest("hex");
 
-    return res.status(200).json({
+    res.status(200).json({
         token,
         expire,
-        signature
+        signature,
+        publicKey: process.env.IMAGEKIT_PUBLIC_KEY
     });
 };
+        
+    
+
